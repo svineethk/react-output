6b4d7f991a28ba8c9ac34243ec4423488297f4da
svineethk <vineethkumar.selvam@gmail.com>
2024-01-10 03:28:27 +0000

    solution

--- src/components/ClickCounter/index.js
+++ src/components/ClickCounter/index.js
@@ -5,12 +5,15 @@ class ClickCounter extends Component {
   state = {count: 0}
 
   render() {
+    const {count} = this.state
     return (
       <div className="bg-container">
-        <h1>The Button has been clicked {this.state.count} times</h1>
+        <h1>The Button has been clicked {count} times</h1>
         <p>Click the button to increase the count!</p>
         <button>Click Me</button>
       </div>
     )
   }
 }
+
+export default ClickCounter
